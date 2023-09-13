767bd5d45e58441c6d6712e9dea4fbc75733f6e2
Ian Frankel <ian.frankel@gmail.com>
2023-09-12 23:03:40 -0400

    looped up

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts)
+
+let messageRecipients = []
+
+function writeCards(messageRecipients, occasion) {
+    let name = "";
+    let notesList = [];
+    for (let i = 0; i < messageRecipients.length; i++) {
+        name = messageRecipients[i];
+        let thanksNote = `Thank you, ${name}, for the wonderful ${occasion} gift!`;
+        notesList.push(thanksNote);
+    }
+    return notesList;
+}
+
+function countDown(n) {
+    while(n >= 0) {
+        console.log(n)
+        n -=1
+    }
+}
